11d8e9721cebfca4f965702aa85806f6765ac1c9
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-06-14 15:30:49 +0200

    Ajout des contrôleurs pour modifier et supprimer des produits, ainsi que mise à jour des routes correspondantes. Amélioration des commentaires dans le code.

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,6 +1,9 @@
 import prisma from "../db/prismaClient.js";
 
-//Controller pour créer un produit
+////////////////////////////////////
+//Controller pour créer un produit//
+//////////////////////////////////////////
+
 export async function createProduct(req, res, next) {
   const { name, description, price, stock } = req.body;
   try {
@@ -23,7 +26,10 @@ export async function createProduct(req, res, next) {
   }
 }
 
-//Controller pour récupérer les produits
+//////////////////////////////////////////
+//Controller pour récupérer les produits//
+/////////////////////////////////////////
+
 export async function getProducts(req, res, next) {
   try {
     const { name, minPrice, maxPrice, stock, id } = req.query;
@@ -59,6 +65,48 @@ export async function getProducts(req, res, next) {
 
     const data = await prisma.product.findMany({ where });
     res.status(200).json({ message: "Produits Trouvé", data });
+  } catch (err) {
+    next(err);
+  }
+}
+
+////////////////////////////////////////////////////////////
+// Controller pour modifier un produit à partir de son id //
+////////////////////////////////////////////////////////////
+
+export async function modifyProduct(req, res, next) {
+  const productId = req.params.id;
+  const { name, description, price, stock } = req.body;
+  try {
+    const data = await prisma.product.update({
+      data: {
+        name: name,
+        description: description,
+        price: price,
+        stock: stock,
+      },
+      where: {
+        id: parseInt(productId),
+      },
+    });
+    res.status(200).json({ message: "Produit modifié", data });
+  } catch (err) {
+    next(err);
+  }
+}
+
+///////////////////////////////////////
+// Controller pour delete un produit //
+///////////////////////////////////////
+export async function deleteProduct(req, res, next) {
+  const productId = req.params.id;
+  try {
+    const data = await prisma.product.delete({
+      where: {
+        id: parseInt(productId),
+      },
+    });
+    res.status(200).json({ message: "Produit supprimé", data });
   } catch (err) {
     next(err);
   }
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import {
   createProduct,
+  deleteProduct,
   getProducts,
+  modifyProduct,
 } from "../controllers/productControllers.js";
 const router = express.Router();
 
 router.post("/create", createProduct);
 router.get("/get", getProducts);
+router.put("/put/:id", modifyProduct);
+router.delete("/delete/:id", deleteProduct);
 
 export default router;
