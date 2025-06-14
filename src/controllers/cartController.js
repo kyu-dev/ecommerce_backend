2b6c7695bd6e68cb730742dc7bb3acd89d2153ea
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-06-14 17:20:13 +0200

    Amélioration du contrôleur de panier : conversion de userId en entier, ajout de l'inclusion des items et des produits liés lors de la récupération du panier. Mise à jour des messages de réponse pour une meilleure clarté en cas de panier vide ou d'utilisateur non trouvé.

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,23 +1,34 @@
 import prisma from "../db/prismaClient.js";
 
 export async function getCart(req, res, next) {
-  const userId = req.params.userId;
+  const userId = parseInt(req.params.userId);
   try {
     const data = await prisma.cart.findUnique({
-      where: {
-        userId: parseInt(userId),
+      where: { userId: userId },
+      include: {
+        items: {
+          // récupère les CartItem liés
+          include: {
+            product: true, // et pour chaque item, récupère aussi le produit lié
+          },
+        },
       },
     });
+
     if (!data) {
-      res.status(200).json({ message: "panié vide" });
-    } else {
-      res.status(200).json({ message: "Récupération du panié", data });
+      return res
+        .status(404)
+        .json({
+          message: "Panier non trouvé, l'utilisateur n'existe peut être pas ?",
+        });
     }
+
+    if (data.items.length === 0) {
+      return res.status(200).json({ message: "Le panier est vide", data });
+    }
+
+    res.status(200).json({ message: "Récupération du panier", data });
   } catch (err) {
     next(err);
   }
-}
-
-export async function addItems(req, res, next) {
-  const userId = req.params.userId;
 }
