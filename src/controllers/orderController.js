7725ed64a533494a04f58911e5bb11566a310627
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-06-17 16:24:48 +0200

    Ajout de la fonctionnalité de création de commande : implémentation du contrôleur pour créer une commande à partir des items du panier et mise à jour des routes pour inclure la méthode POST.

---
+++ src/controllers/orderController.js
@@ -0,0 +1,52 @@
+import prisma from "../db/prismaClient.js";
+
+export async function createOrder(req, res, next) {
+  const userId = parseInt(req.params.userId);
+  try {
+    const cart = await prisma.cart.findUnique({
+      where: {
+        userId: userId,
+      },
+      include: {
+        items: {
+          include: {
+            product: true,
+          },
+        },
+      },
+    });
+
+    const order = prisma.order.create({
+      data: {
+        userId,
+        orderItems: {
+          create: cart.items.map((item) => ({
+            productId: item.productId,
+            quantity: item.quantity,
+            price: item.product.price,
+          })),
+        },
+      },
+      include: {
+        orderItems: true,
+      },
+    });
+    await prisma.cartItem.deleteMany({
+      where: { cartId: cart.id },
+    });
+
+    //CALCULE du total de la comande
+    const total = cart.items.reduce(
+      (acc, item) => acc + item.quantity * item.product.price,
+      0
+    );
+
+    res.status(201).json({
+      message: "Commande créée avec succès.",
+      order,
+      total,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { createOrder } from "../controllers/orderController.js";
 const router = express.Router();
 
+router.post("/:userId", createOrder);
 export default router;
