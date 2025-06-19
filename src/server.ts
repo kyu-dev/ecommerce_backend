cbaa2516ee735220940f5075754689d431c182f7
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-06-19 16:32:28 +0200

    Ajout de Swagger pour la documentation de l'API, incluant la configuration et l'intégration dans le serveur.

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,27 @@ import passport from "./auth/passport";
 import cart from "./routes/cartRoutes";
 import order from "./routes/orderRoutes";
 import { errorHandler } from "./middleware/errorHandler";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Ecommerce",
+      version: "1.0.0",
+      description: "Documentation de l'API Ecommerce",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts", "./src/controllers/*.ts"],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 const app = express();
 const PORT = 3000;
@@ -16,6 +37,7 @@ app.use("/authentication", auth);
 app.use("/product", product);
 app.use("/cart", cart);
 app.use("/order", order);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
   console.log(`✅ Serveur lancé sur http://localhost:${PORT}`);
