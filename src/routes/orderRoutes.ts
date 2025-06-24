a5078dfc51a771f1fbd963b24a86ebbd52af49fa
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-06-24 10:10:19 +0200

    API Ecommerce

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -2,6 +2,36 @@ import express from "express";
 import { createOrder, getOrders } from "../controllers/orderController";
 const router = express.Router();
 
+/**
+ * @swagger
+ * /order/{userId}:
+ *   post:
+ *     summary: Créer une commande pour un utilisateur
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de l'utilisateur
+ *     responses:
+ *       201:
+ *         description: Commande créée avec succès
+ *       400:
+ *         description: Panier vide ou inexistant
+ *   get:
+ *     summary: Récupérer les commandes d'un utilisateur
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de l'utilisateur
+ *     responses:
+ *       200:
+ *         description: Commandes récupérées
+ */
 router.post("/:userId", createOrder);
 router.get("/:userId", getOrders);
 export default router;
