5a1b6b1eff8bc45d9ad94f8dcfff19d96db176ec
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-06-25 10:00:00 +0200

    swagger pour les routes de panier

--- src/routes/cartRoutes.ts
+++ src/routes/cartRoutes.ts
@@ -15,4 +15,105 @@ router.delete("/:userId/item/:productId", deleteItem);
 router.put("/:userId", modifyProduct);
 router.delete("/:userId/clearCart", clearCart);
 
+/**
+ * @swagger
+ * /cart/{userId}:
+ *   get:
+ *     summary: Récupérer le panier d'un utilisateur
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de l'utilisateur
+ *     responses:
+ *       200:
+ *         description: Panier récupéré
+ *   post:
+ *     summary: Ajouter des articles au panier
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de l'utilisateur
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               productId:
+ *                 type: integer
+ *                 example: 1
+ *               quantity:
+ *                 type: integer
+ *                 example: 2
+ *     responses:
+ *       200:
+ *         description: Article(s) ajouté(s)
+ *   put:
+ *     summary: Modifier un produit du panier
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de l'utilisateur
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               productId:
+ *                 type: integer
+ *                 example: 1
+ *               quantity:
+ *                 type: integer
+ *                 example: 3
+ *     responses:
+ *       200:
+ *         description: Produit modifié
+ *
+ * /cart/{userId}/item/{productId}:
+ *   delete:
+ *     summary: Supprimer un article du panier
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de l'utilisateur
+ *       - in: path
+ *         name: productId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID du produit
+ *     responses:
+ *       200:
+ *         description: Article supprimé
+ *
+ * /cart/{userId}/clearCart:
+ *   delete:
+ *     summary: Vider le panier
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de l'utilisateur
+ *     responses:
+ *       200:
+ *         description: Panier vidé
+ */
+
 export default router;
