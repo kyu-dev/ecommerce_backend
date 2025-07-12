d3d36022f2d5e7c5d96b2f0a55e8c6a4d54c5bb2
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-07-12 10:48:55 +0200

    fix: suppression de la configuration du domaine pour le partage des cookies dans les fonctions de connexion et de rappel Google

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -32,7 +32,6 @@ export function login(req, res, next) {
       secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      domain: ".vercel.app", // Partage le cookie avec tous les sous-domaines vercel.app
     });
     return res.json({ message: "hmmm le bon chocoCookie", token });
   })(req, res, next);
@@ -82,7 +81,6 @@ export function googleCallback(req, res) {
     secure: true,
     sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    domain: ".vercel.app", // Partage le cookie avec tous les sous-domaines vercel.app
   });
 
   res.redirect(
